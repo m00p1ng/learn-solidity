945ff66adbbf01695d8fa9581b67d7e7034ccfca
mooping <mongkonchai4412@gmail.com>
2018-08-12 16:13:20 +0700

    add lottery frontend

---
+++ lottery-react/src/lottery.js
@@ -0,0 +1,7 @@
+import web3 from './web3';
+
+const address = '0xDe88aDB7Bc0fa6315Ba428bE12b8ab98aaFc857E';
+
+const abi = [{ "constant": true, "inputs": [], "name": "manager", "outputs": [{ "name": "", "type": "address" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [], "name": "pickWinner", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "getPlayers", "outputs": [{ "name": "", "type": "address[]" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [], "name": "enter", "outputs": [], "payable": true, "stateMutability": "payable", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "uint256" }], "name": "players", "outputs": [{ "name": "", "type": "address" }], "payable": false, "stateMutability": "view", "type": "function" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor" }];
+
+export default new web3.eth.Contract(abi, address);
